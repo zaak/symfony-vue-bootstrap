565ddf3702c85261d5ec69cf4e995f98af19477f
Paweł Wiaderny <zaak@users.noreply.github.com>
2022-03-15 07:40:59 +0100

    Update dependencies

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
     public function index()
     {
         return $this->render('index.html.twig');
